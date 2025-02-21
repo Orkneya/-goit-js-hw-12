7c01bc89d9fcd3e69dd88f6c9afb99f2d5e9b814
Popenko Tetiana <orkneya17@gmail.com>
2025-02-21 22:43:52 +0200

    first

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '48810135-3fc2af65eea4986756bc51882';
+const BASE_URl="https://pixabay.com/api/?";
+
+ export function searchImging(name){
+  const params = new URLSearchParams({
+    key : API_KEY ,
+    q : name,
+    image_type : "photo",
+    orientation : "horizontal",
+    safesearch : true,
+  });
+  return axios.get(`${BASE_URl}${params}`)
+  }
---
+++ src/main.js
@@ -0,0 +1,48 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { searchImging } from "./js/pixabay-api";
+import { renderItems } from "./js/render-functions";
+export const refs={
+  container : document.querySelector(".gallery"),
+  formSubmit: document.querySelector(".form"),
+  loader : document.querySelector(".loader"),
+  btnChange :document.querySelector(".btn_down"),
+}
+const mes1 ="Fill please all fields!";
+const mes2 = "Sorry, there are no images matching your search query. Please, try again!";
+
+refs.formSubmit.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e){
+  e.preventDefault();
+  const inputValue = e.currentTarget.elements.inpImg.value;
+  if (!inputValue.trim()){
+    showError(mes1);
+    return console.log(mes1);
+  }
+    refs.loader.style.display = 'block';
+    searchImging(inputValue)
+    .then(response=>{
+      if (response.data.hits.length === 0){
+        refs.container.innerHTML = '';
+        showError(mes2);
+        refs.loader.style.display = 'none';
+        return console.log(mes2);
+      } 
+      renderItems(response.data.hits);
+      refs.loader.style.display = 'none';
+    })
+    .catch(error=>{
+      showError(mes2);
+      return console.log("Error:", error);
+    });
+    e.target.reset();
+};
+
+export function showError(msg){
+  iziToast.show({
+        message: msg,
+        position:"topRight",
+        messageColor: 'white',
+        backgroundColor: "#cb4335",
+        });
+}
